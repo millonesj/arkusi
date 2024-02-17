41a39167a137154ab22166ad15285bbe14861e56
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2024-02-16 23:01:21 -0500

    refactor: build page

---
+++ _next/static/oZ03h53tnPUIuQCrYlOGu/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcategory\u002F[slug]","\u002Fcontacto","\u002Fnosotros","\u002Fposts","\u002Fposts\u002F[slug]","\u002Fprojectos","\u002Fprojectos\u002F[slug]","\u002Ftag\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
