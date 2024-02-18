6a3247fa8bbfee0e7be20e66fae79bc330926d19
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2024-02-17 22:05:26 -0500

    fix(project): set navigation links

--- src/components/projects/project-page-navigation.js
+++ src/components/projects/project-page-navigation.js
@@ -7,7 +7,7 @@ function ProjectPageNavigation(props) {
         <div className="page-navigation">
             <div className="image-layer relative">
                 <div className="grid grid-cols-2 relative text-[18px] leading-6 uppercase font-semibold tracking-[10px] z-[1]">
-                    <Link href={`/projects/${prevProject.slug}`}>
+                    <Link href={`/proyectos/${prevProject.slug}`}>
                         <a
                             className={`prev py-[50px] mr-[30px] flex justify-center bg-cover bg-no-repeat bg-center relative z-[1] before:z-[-1] before:bg-black before:opacity-60 before:absolute before:w-full before:h-full before:top-0 before:left-0 ${
                                 !prevProject?.slug
@@ -21,7 +21,7 @@ function ProjectPageNavigation(props) {
                             Prev
                         </a>
                     </Link>
-                    <Link href={`/projects/${nextProject?.slug}`}>
+                    <Link href={`/proyectos/${nextProject?.slug}`}>
                         <a
                             className={`next py-[50px] flex justify-center bg-cover bg-no-repeat bg-center text-right relative z-[1] before:z-[-1] before:bg-black before:opacity-60 before:absolute before:w-full before:h-full before:top-0 before:left-0 ${
                                 !nextProject?.slug
